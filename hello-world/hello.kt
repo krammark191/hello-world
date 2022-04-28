9c897a27d9911c01ed36c4d54232f78660d5b32d
Mark Van Horn <krammark191@gmail.com>
2022-04-28 13:51:13 -0600

    I don't even know, man

---
+++ hello-world/hello.kt
@@ -0,0 +1,11 @@
+fun main() {
+    println("Hello, world!")
+}
+
+
+// To run use following commands
+// kotlinc filename.kt -d filename.jar
+// java -jar filename.jar
+
+// kotlinc hello-world/hello.kt -d hello.jar
+// java -jar .\hello.jar
